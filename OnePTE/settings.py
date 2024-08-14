c67b523a12972f2226ae0af865b2835892613c0b
hsakash <hemelakash472@gmail.com>
2024-08-14 11:18:25 +0600

    PostgreSQL connection settings

--- OnePTE/settings.py
+++ OnePTE/settings.py
@@ -79,10 +79,23 @@ WSGI_APPLICATION = 'OnePTE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# PostgreSQL database configuration
+# https://docs.djangoproject.com/en/5.0/ref/databases/#postgresql-notes
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
