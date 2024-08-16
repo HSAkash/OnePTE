f5e2ac1d8a2b38c8004e6362d078c5743a1bada3
hsakash <hemelakash472@gmail.com>
2024-08-16 10:08:36 +0600

    reduce wait_for_db time

--- user/management/commands/wait_for_db.py
+++ user/management/commands/wait_for_db.py
@@ -17,6 +17,6 @@ class Command(BaseCommand):
                 db_conn = connections['default']
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(10)
+                time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
